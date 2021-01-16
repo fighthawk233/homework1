4c5713ba8c18521d713f0c75bf3d914d78d62bd7
明宇 陈 <1527658407@qq.com>
2021-01-16 19:07:27 +0800

    添加项目文件。

---
+++ 源.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#define DAYS_PER_YEAR 365
+int main(void)
+{
+	/*printf("³ÂÃ÷Óî\n");
+	printf("Ã÷Óî\n³Â\n");
+	printf("³Â"); printf("Ã÷Óî\n");*/
+
+	/*int age=18, days;
+	days =DAYS_PER_YEAR* age;
+	printf("age=%d,days=%d\n", age, days);*/
+
+	//jolly(); jolly(); jolly(); deny();
+
+	/*br(); printf(","); ic(); printf("\n");
+	ic(); printf("\n");
+	br(); printf("\n");*/
+
+	//int toes = 10;
+	//printf("toes=%d,2*toes=%d,toes squared=%d\n", toes, toes * 2, toes*toes);
+
+	//s(); s(); s(); printf("\n");
+	//s(); s(); printf("\n");
+	//s(); printf("\n");
+
+	/*printf("starting now:\n");
+	one_three();
+	printf("done!\n");*/
+
+	getchar();
+	return 0;
+}
+/*int jolly(void)
+{
+	printf("For he's a jolly good fellow!\n");
+}
+int deny(void)
+{
+	printf("Which nobody can deny!\n");
+}*/
+
+/*int br(void)
+{
+	printf("Brazil,Russia");
+}
+int ic(void)
+{
+	printf("India,China");
+}*/
+
+/*int s(void)
+{
+	printf("Smile!");
+}*/
+
+/*int one_three(void)
+{
+	printf("one\n");
+	two();
+	printf("three\n");
+}
+int two(void)
+{
+	printf("two\n");
+}*/
